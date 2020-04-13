81b1326c3c189e77b8bb5257b838d507cdd93846
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-13 22:26:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -17,7 +17,11 @@ def input_to_index(input)
   index = (input.to_i) - 1
 end
 
-# Calculating the user's move
+# Displaying
 def move(board, index, token_choice="X")
-
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
