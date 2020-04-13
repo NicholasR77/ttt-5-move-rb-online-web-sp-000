dd6178bbcb546777492293c9e387cebc5f09cf51
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-13 22:26:31 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -19,6 +19,7 @@ end
 
 # Displaying
 def move(board, index, token_choice="X")
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
